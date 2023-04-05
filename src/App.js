c0a15a34de345885f68912aadb48548bbeff7be8
simeonYanev <simeon.yanev1@gmail.com>
2023-04-05 11:16:48 +0300

    ADD: Sidebar

--- src/App.js
+++ src/App.js
@@ -1,18 +1,21 @@
 import React from 'react';
 import './App.css';
 import Header from './Header';
+import Sidebar from './Sidebar';
 
 function App() {
   return (
     <div className="app">
 
-      {/* Header */}
       <Header />
 
       {/* App Body */}
-        {/* Sidebar */}
+      <div className='app_body'>
+        <Sidebar />
         {/* Feed */}
         {/* Widgets */}
+      </div>
+
     </div>
   );
 }
