3974d1e6f00f14a784f40687604f9e2073b2b7d1
simeonYanev <simeon.yanev1@gmail.com>
2023-04-05 11:11:42 +0300

    ADD: Header

--- src/Header.js
+++ src/Header.js
@@ -3,6 +3,9 @@ import './Header.css';
 import SearchIcon from '@mui/icons-material/Search';
 import HomeIcon from '@mui/icons-material/Home';
 import SupervisorAccountIcon from '@mui/icons-material/SupervisorAccount';
+import BusinessCenterIcon from '@mui/icons-material/BusinessCenter';
+import ChatIcon from '@mui/icons-material/Chat';
+import NotificationsIcon from '@mui/icons-material/Notifications';
 import HeaderOption from './HeaderOption';
 
 function Header() {
@@ -21,6 +24,10 @@ function Header() {
         <div className='header__right'>
             <HeaderOption Icon={HomeIcon} title="Home"/>
             <HeaderOption Icon={SupervisorAccountIcon} title="My Network"/>
+            <HeaderOption Icon={BusinessCenterIcon} title="Jobs"/>
+            <HeaderOption Icon={ChatIcon} title="Messaging"/>
+            <HeaderOption Icon={NotificationsIcon} title="Notification"/>
+            <HeaderOption avatar={"https://media.licdn.com/dms/image/D5603AQHQAQTjKGvckg/profile-displayphoto-shrink_400_400/0/1680446605259?e=1686182400&v=beta&t=XxrCMaqrGxnPtdnxl4SZX80fi6EfX5t5WZP1E-jVRIM"} title="Me" />
         </div>
     </div>
   )
--- src/HeaderOption.js
+++ src/HeaderOption.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import "./HeaderOption.css";
+import Avatar from '@mui/material/Avatar';
 
-function HeaderOption({ Icon, title}) {
+function HeaderOption({ avatar, Icon, title}) {
   return (
     <div className='headerOption'>
         {Icon && <Icon className="headerOption__icon" />}
+        {avatar && (<Avatar className="headerOption__icon" src={avatar}/>)}
         <h3 className='headerOption__title'>{title}</h3>
     </div>
   )
