46471c9807b06f78b7e1df1b15c3ae1c6dedb0a9
simeonYanev <simeon.yanev1@gmail.com>
2023-04-05 09:24:46 +0300

    ADD: Header

---
+++ src/Header.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import './Header.css';
+import SearchIcon from '@mui/icons-material/Search';
+
+function Header() {
+  return (
+    <div className='header'>
+        <h1>Header</h1>
+
+        <div className='header__left'>
+            <img src='https://upload.wikimedia.org/wikipedia/commons/thumb/c/ca/LinkedIn_logo_initials.png/800px-LinkedIn_logo_initials.png' alt='' />
+
+            <div className='header_search'>
+            <SearchIcon />
+            <input type='text' />
+            </div>
+        </div>
+
+        <div className='header__right'>
+
+        </div>
+    </div>
+  )
+}
+
+export default Header
