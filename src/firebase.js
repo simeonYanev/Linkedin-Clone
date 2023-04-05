40351208d939656667b7dabfd00eeba905c743e7
simeonYanev <simeon.yanev1@gmail.com>
2023-04-05 08:56:30 +0300

    ADD: initial commit

---
+++ src/firebase.js
@@ -0,0 +1,12 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCL2OsIXQV-86FBb87VxnuvbLTDk-tI_ww",
+  authDomain: "linkedin-clone-6922e.firebaseapp.com",
+  projectId: "linkedin-clone-6922e",
+  storageBucket: "linkedin-clone-6922e.appspot.com",
+  messagingSenderId: "30673375847",
+  appId: "1:30673375847:web:611d907e5f611bfe99fac2"
+};
+
+const app = initializeApp(firebaseConfig);
