1888d384e64eaed7902993fff84c8916ca046f9a
simeonYanev <simeon.yanev1@gmail.com>
2023-04-06 13:20:20 +0300

    ADD: Feed

--- src/Feed.js
+++ src/Feed.js
@@ -1,6 +1,7 @@
-import React from 'react';
+import React, { useState } from 'react';
 import "./Feed.css";
 import InputOption from './InputOption';
+import Post from './Post';
 import CreateIcon from '@mui/icons-material/Create';
 import ImageIcon from '@mui/icons-material/Image';
 import SubscriptionsIcon from '@mui/icons-material/Subscriptions';
@@ -8,6 +9,15 @@ import EventNoteIcon from '@mui/icons-material/EventNote';
 import CalendarViewDayIcon from '@mui/icons-material/CalendarViewDay';
 
 function Feed() {
+
+  const [posts, setPosts] = useState([]);
+
+  const sendPost = (e) => {
+    e.preventDefault();
+
+    
+  }
+
   return (
     <div className='feed'>
 
@@ -17,18 +27,28 @@ function Feed() {
           <CreateIcon />
           <form>
             <input type='text' />
-            <button type='submit'>Send</button>
+            <button onClick={sendPost} type='submit'>Send</button>
           </form>
         </div>
 
         <div className='feed__inputOptions'>
-          <InputOption Icon={ImageIcon} title="Photo" color="#70B5F9"/>
-          <InputOption Icon={SubscriptionsIcon} title="Video" color="#61BE7E"/>
-          <InputOption Icon={EventNoteIcon} title="Event" color="#E7A33E"/>
-          <InputOption Icon={CalendarViewDayIcon} title="Write article" color="#E98F70"/>
+          <InputOption Icon={ImageIcon} title="Photo" color="#70B5F9" />
+          <InputOption Icon={SubscriptionsIcon} title="Video" color="#61BE7E" />
+          <InputOption Icon={EventNoteIcon} title="Event" color="#E7A33E" />
+          <InputOption Icon={CalendarViewDayIcon} title="Write article" color="#E98F70" />
         </div>
 
       </div>
+
+      {/* Posts */}
+      {posts.map((post) => (
+        <Post />
+      ))}
+      <Post
+        name="Simeon Yanev"
+        description="Test description"
+        message="This is workings  asdasdasda "
+      />
 
     </div>
   )
---
+++ src/Post.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import "./Post.css";
+import { Avatar } from '@mui/material';
+import InputOption from './InputOption';
+import ThumbUpAltIcon from '@mui/icons-material/ThumbUpAlt';
+import ChatOutlinedIcon from '@mui/icons-material/ChatOutlined';
+import ShareOutlinedIcon from '@mui/icons-material/ShareOutlined';
+import SendOutlinedIcon from '@mui/icons-material/SendOutlined';
+
+function Post({ name, description, message, photoUrl }) {
+  return (
+    <div className='post'>
+
+      {/* Post Header */}
+      <div className='post__header'>
+        <Avatar />
+
+        <div className='post__info'>
+          <h2>{name}</h2>
+          <p>{description}</p>
+        </div>
+
+      </div>
+
+      {/* Post Body */}
+      <div className='post__body'>
+        <p>{message}</p>
+      </div>
+
+      {/* Post Buttons */}
+      <div className='post__buttons'>
+        <InputOption Icon={ThumbUpAltIcon} title="Like" color="gray" />
+        <InputOption Icon={ChatOutlinedIcon} title="Comment" color="gray" />
+        <InputOption Icon={ShareOutlinedIcon} title="Share" color="gray" />
+        <InputOption Icon={SendOutlinedIcon} title="Send" color="gray" />
+      </div>
+
+    </div>
+  )
+}
+
+export default Post;
