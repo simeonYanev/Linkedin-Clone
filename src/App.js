81a111bdf3b058f47730466a7e0317a3ee65ef72
simeonYanev <simeon.yanev1@gmail.com>
2023-04-07 12:33:25 +0300

    ADD: Login

--- src/App.js
+++ src/App.js
@@ -1,15 +1,36 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import './App.css';
 import Header from './Header';
 import Sidebar from './Sidebar';
 import Feed from './Feed';
 import Login from './Login';
-import { useSelector } from 'react-redux';
-import { selectUser } from './features/userSlice';
+import { useDispatch, useSelector } from 'react-redux';
+import { login, logout, selectUser } from './features/userSlice';
+import { auth } from './firebase';
 
 function App() {
 
   const user = useSelector(selectUser);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    auth.onAuthStateChanged(userAuth => {
+      if (userAuth) {
+        // user is logged in
+        dispatch(
+          login({
+            email: userAuth.email,
+            uid: userAuth.uid,
+            displayName: userAuth.displayName,
+            photoUrl: userAuth.photoURL,
+          })
+        );
+      } else {
+        // user is logged out
+        dispatch(logout());
+      }
+    });
+  }, []);
 
   return (
     <div className="app">
--- src/Header.js
+++ src/Header.js
@@ -7,8 +7,19 @@ import BusinessCenterIcon from '@mui/icons-material/BusinessCenter';
 import ChatIcon from '@mui/icons-material/Chat';
 import NotificationsIcon from '@mui/icons-material/Notifications';
 import HeaderOption from './HeaderOption';
+import { useDispatch } from 'react-redux';
+import { logout } from './features/userSlice';
+import { auth } from './firebase';
 
 function Header() {
+
+  const dispatch = useDispatch();
+
+  const logoutOfApp = () => {
+    dispatch(logout())
+    auth.signOut();
+  };
+
   return (
     <div className='header'>
 
@@ -27,7 +38,7 @@ function Header() {
             <HeaderOption Icon={BusinessCenterIcon} title="Jobs"/>
             <HeaderOption Icon={ChatIcon} title="Messaging"/>
             <HeaderOption Icon={NotificationsIcon} title="Notification"/>
-            <HeaderOption avatar={"https://media.licdn.com/dms/image/D5603AQHQAQTjKGvckg/profile-displayphoto-shrink_400_400/0/1680446605259?e=1686182400&v=beta&t=XxrCMaqrGxnPtdnxl4SZX80fi6EfX5t5WZP1E-jVRIM"} title="Me" />
+            <HeaderOption onClick={logoutOfApp} avatar={"https://media.licdn.com/dms/image/D5603AQHQAQTjKGvckg/profile-displayphoto-shrink_400_400/0/1680446605259?e=1686182400&v=beta&t=XxrCMaqrGxnPtdnxl4SZX80fi6EfX5t5WZP1E-jVRIM"} title="Me" />
         </div>
     </div>
   )
