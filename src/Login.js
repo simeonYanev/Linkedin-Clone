b9a8a48d9a1f4259d4539fd37a69afee122c27c3
simeonYanev <simeon.yanev1@gmail.com>
2023-04-07 09:18:15 +0300

    ADD: Login

--- src/Login.js
+++ src/Login.js
@@ -1,14 +1,44 @@
-import React from 'react';
+import React, { useState } from 'react';
 import "./Login.css";
+import { auth } from "./firebase"
+import { useDispatch } from 'react-redux';
+import { login } from './features/userSlice';
 
 function Login() {
 
-    const loginToApp = () => {
-        
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [name, setName] = useState("");
+    const [picture, setPicture] = useState("");
+    const dispatch = useDispatch();
+
+
+    const loginToApp = (e) => {
+        e.preventDefault();
     };
 
     const register = () => {
+        if (!name) {
+            return alert("Please enter your full name!");
+        }
 
+        auth.createUserWithEmailAndPassword(email, password)
+            .then(
+                (userAuth) => {
+                    userAuth.user.updateProfile({
+                        displayName: name,
+                        photoURL: picture,
+                    })
+                        .then(() => {
+                            dispatch(
+                                login({
+                                    email: userAuth.user.email,
+                                    uid: userAuth.user.uid,
+                                    displayName: name,
+                                    photoURL: picture
+                                }));
+                        });
+                }).catch((error) => alert(error.message));
     };
 
     return (
@@ -17,13 +47,33 @@ function Login() {
             <img src='https://logos-world.net/wp-content/uploads/2020/04/Linkedin-Logo.png' alt='' />
 
             <form>
-                <input placeholder='Full Name (required if registering)' type='text' />
+                <input
+                    value={name}
+                    onChange={e => setName(e.target.value)}
+                    placeholder='Full Name (required if registering)'
+                    type='text'
+                />
 
-                <input placeholder='Profile picture URL (optional)' type='text' />
+                <input
+                    placeholder='Profile picture URL (optional)'
+                    value={picture}
+                    onChange={e => setPicture(e.target.value)}
+                    type='text'
+                />
 
-                <input placeholder='Email' type='email' />
+                <input
+                    value={email}
+                    onChange={e => setEmail(e.target.value)}
+                    placeholder='Email'
+                    type='email'
+                />
 
-                <input placeholder='Password' type='password' />
+                <input
+                    value={password}
+                    onChange={e => setPassword(e.target.value)}
+                    placeholder='Password'
+                    type='password'
+                />
 
                 <button type='submit' onClick={loginToApp}>Sign In</button>
             </form>
