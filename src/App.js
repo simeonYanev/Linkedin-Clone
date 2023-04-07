6b9108b2ce6212bac83b9ab46f68106e2d8e49a2
simeonYanev <simeon.yanev1@gmail.com>
2023-04-07 07:38:03 +0300

    ADD: App

--- src/App.js
+++ src/App.js
@@ -3,19 +3,28 @@ import './App.css';
 import Header from './Header';
 import Sidebar from './Sidebar';
 import Feed from './Feed';
+import Login from './Login';
+import { useSelector } from 'react-redux';
+import { selectUser } from './features/userSlice';
 
 function App() {
+
+  const user = useSelector(selectUser);
+
   return (
     <div className="app">
 
       <Header />
 
-      {/* App Body */}
-      <div className='app__body'>
-        <Sidebar />
-        <Feed />
-        {/* Widgets */}
-      </div>
+      {!user ? (
+        <Login />
+      ) : (
+        <div className='app__body'>
+          <Sidebar />
+          <Feed />
+          {/* Widgets */}
+        </div>
+      )}
 
     </div>
   );
