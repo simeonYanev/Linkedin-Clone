a938eccb4d75d69073bfc4a3686f84529e5966bf
simeonYanev <simeon.yanev1@gmail.com>
2023-04-07 06:41:58 +0300

    ADD: Feed

--- src/Feed.js
+++ src/Feed.js
@@ -8,28 +8,38 @@ import SubscriptionsIcon from '@mui/icons-material/Subscriptions';
 import EventNoteIcon from '@mui/icons-material/EventNote';
 import CalendarViewDayIcon from '@mui/icons-material/CalendarViewDay';
 import { db } from './firebase';
-import { collection } from 'firebase/firestore';
-
+import firebase from 'firebase/compat/app';
 
 function Feed() {
 
+  const [input, setInput] = useState([]);
   const [posts, setPosts] = useState([]);
 
   useEffect(() => {
-    db.collection("posts").onSnapshot((snapshot) =>
-      setPosts(
-        snapshot.docs.map((doc) => ({
-          id: doc.id,
-          data: doc.data(),
-        }))
-      )
-    );
+    db.collection("posts")
+      .orderBy("timestamp", "desc")
+      .onSnapshot((snapshot) =>
+        setPosts(
+          snapshot.docs.map((doc) => ({
+            id: doc.id,
+            data: doc.data(),
+          }))
+        )
+      );
   }, []);
 
   const sendPost = (e) => {
     e.preventDefault();
 
+    db.collection("posts").add({
+      name: "Simeon Yanev",
+      description: "This is a test",
+      message: input,
+      photoUrl: "",
+      timestamp: firebase.firestore.FieldValue.serverTimestamp()
+    });
 
+    setInput("");
   }
 
   return (
@@ -40,7 +50,7 @@ function Feed() {
         <div className='feed__input'>
           <CreateIcon />
           <form>
-            <input type='text' />
+            <input value={input} onChange={e => setInput(e.target.value)} type='text' />
             <button onClick={sendPost} type='submit'>Send</button>
           </form>
         </div>
@@ -55,14 +65,15 @@ function Feed() {
       </div>
 
       {/* Posts */}
-      {posts.map((post) => (
-        <Post />
+      {posts.map(({ id, data: { name, description, message, photoUrl } }) => (
+        <Post
+          key={id}
+          name={name}
+          description={description}
+          message={message}
+          photoUrl={photoUrl}
+        />
       ))}
-      <Post
-        name="Simeon Yanev"
-        description="Test description"
-        message="This is workings  asdasdasda "
-      />
 
     </div>
   )
--- src/firebase.js
+++ src/firebase.js
@@ -1,6 +1,6 @@
-import { initializeApp } from "firebase/app";
-import { getFirestore } from "firebase/firestore";
-import { getAuth } from "firebase/auth";
+import firebase from "firebase/compat/app";
+import "firebase/compat/auth";
+import "firebase/compat/firestore";
 
 const firebaseConfig = {
   apiKey: "AIzaSyCL2OsIXQV-86FBb87VxnuvbLTDk-tI_ww",
@@ -11,8 +11,8 @@ const firebaseConfig = {
   appId: "1:30673375847:web:611d907e5f611bfe99fac2"
 };
 
-const app = initializeApp(firebaseConfig);
-const db = getFirestore(app);
-const auth = getAuth(app);
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+const db = firebaseApp.firestore();
+const auth = firebase.auth();
 
-export {db, auth};
+export { db, auth };
