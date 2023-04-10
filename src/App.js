ba69face8578a595c9d5912aa3272624e1245d09
simeonYanev <simeon.yanev1@gmail.com>
2023-04-10 13:30:22 +0300

    ADD: App

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Header from './Header';
 import Sidebar from './Sidebar';
 import Feed from './Feed';
 import Login from './Login';
+import Widgets from './Widgets';
 import { useDispatch, useSelector } from 'react-redux';
 import { login, logout, selectUser } from './features/userSlice';
 import { auth } from './firebase';
@@ -43,7 +44,7 @@ function App() {
         <div className='app__body'>
           <Sidebar />
           <Feed />
-          {/* Widgets */}
+          <Widgets />
         </div>
       )}
 
